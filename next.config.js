4246551642eaed4a91cfc1d865f5f7f20cd943f3
7ma7X <7X.rusk@gmail.com>
2019-01-25 10:27:08 +0900

    keybase.txtの配置

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+const fs = require('fs')
+
+module.exports = withCSS({
+  exportPathMap: async function () {
+    fs.copyFileSync('./static/keybase.txt', './out/keybase.txt')
+
+    return {
+      '/': { page: '/' },
+      '/whoami': { page: '/whoami' },
+      '/404.html': { page: '/_error' },
+    }
+  }
+})
