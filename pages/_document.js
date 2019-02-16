c51fc186d31224269a5d920e4f6710d1353a61db
7ma7X <7X.rusk@gmail.com>
2019-02-16 23:29:51 +0900

    meta edit

--- pages/_document.js
+++ pages/_document.js
@@ -14,9 +14,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="theme-color" content="#000000" />
           <meta name="description" content="HelloRusk Official Website." />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@HelloRusk" />
-          <meta property="og:image" content="/static/mika_square.jpg" />
+          <meta property="og:image" content="/static/mika_square.png" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:300" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400" />
         </Head>
