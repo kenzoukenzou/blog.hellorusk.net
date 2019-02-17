255fa2e475623df65437ffd6dc275bdc9c08192d
7ma7X <7X.rusk@gmail.com>
2019-02-17 21:57:37 +0900

    check command

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const withMDX = require("@zeit/next-mdx")();
 
 module.exports = withMDX({
@@ -12,25 +13,30 @@ module.exports = withMDX({
     const data = {
       postsDateList: posts
     };
-    fs.writeFileSync("./out/posts.json", JSON.stringify(data));
 
-    const pathMap = {};
-    pathMap["/"] = { page: "/" };
-    pathMap["/whoami"] = { page: "/whoami" };
-    pathMap["/blog"] = { page: "/blog" };
-    pathMap["404.html"] = { page: "/_error" };
+    fs.writeFile("./static/posts.json", JSON.stringify(data), err => {
+      if (err) throw err;
 
-    for (const post of posts) {
-      const postPath = post.replace(/(\d{4})(\d{2})(\d{2})\.mdx/, (match, p1, p2, p3) => {
-        return `/blog/${p1}/${p2}/${p3}`;
-      });
-      const pagePath = post.replace(/(\d{4})(\d{2})(\d{2})\.mdx/, (match, p1, p2, p3) => {
-        return `/posts/${p1}${p2}${p3}`;
-      });
-      pathMap[postPath] = { page: pagePath };
-    }
- 
-    console.log(pathMap);
-    return pathMap;
+      console.log(`your posts data has been saved in ${path.resolve("./static/posts.json")}`);
+
+      const pathMap = {};
+      pathMap["/"] = { page: "/" };
+      pathMap["/whoami"] = { page: "/whoami" };
+      pathMap["/blog"] = { page: "/blog" };
+      pathMap["404.html"] = { page: "/_error" };
+  
+      for (const post of posts) {
+        const postPath = post.replace(/(\d{4})(\d{2})(\d{2})\.mdx/, (match, p1, p2, p3) => {
+          return `/blog/${p1}/${p2}/${p3}`;
+        });
+        const pagePath = post.replace(/(\d{4})(\d{2})(\d{2})\.mdx/, (match, p1, p2, p3) => {
+          return `/posts/${p1}${p2}${p3}`;
+        });
+        pathMap[postPath] = { page: pagePath };
+      }
+   
+      console.log(pathMap);
+      return pathMap;
+    });
   }
 });
