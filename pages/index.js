83d2dc9cb569a29d7a59c994d44cfc7c35e74c45
7ma7X <7X.rusk@gmail.com>
2019-02-20 19:46:55 +0900

    fix position in .links ul

--- pages/index.js
+++ pages/index.js
@@ -175,6 +175,7 @@ const renderInfo = (num) => {
         .links ul {
           margin: 0 30px 0 0;
           list-style-type: none;
+          padding-inline-start: 3em;
         }
 
         .links li {
