a06fcd82720a97da40512102728ac65d715457e2
7ma7X <7X.rusk@gmail.com>
2019-05-15 22:15:53 +0900

    edit form style

--- pages/form.js
+++ pages/form.js
@@ -7,24 +7,36 @@ export default () => (
     <Meta>
       <title key="title">form - HelloRusk Official Website</title>
     </Meta>
-    <form name="contact" action="/" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="form-name" value="contact" />
-      <input type="hidden" name="bot-field" />
-      <p>
-        <label>Name: <br /><textarea name="name" className="name" required></textarea></label>
-      </p>
-      <p>
-        <label>Message: <br /><textarea name="message" className="message" required></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <div className="form-container">
+      <form name="contact" action="/" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="bot-field" />
+        <p>
+          <label>Name: <br /><textarea name="name" className="name" required></textarea></label>
+        </p>
+        <p>
+          <label>Message: <br /><textarea name="message" className="message" required></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+        <br />
+      </form>
+    </div>
     <div className="jump">
       <Link scroll={false} href="/">
         <a>home</a>
       </Link>
     </div>
     <Footer />
+    <style jsx>{`
+      .form-container {
+        text-align: center;
+      }
+
+      form {
+        display: inline-block;
+      }
+    `}</style>
   </div>
 );
