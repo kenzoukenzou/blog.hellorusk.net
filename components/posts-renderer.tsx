f20a8eea8c9fbe29a5070411ea3c71f88efa372f
7ma7X <hellorusk1998@gmail.com>
2019-11-25 23:19:29 +0900

    modify post-renderer

--- components/posts-renderer.tsx
+++ components/posts-renderer.tsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 import data from "./posts.json";
 import { useState } from "react";
 import { internalLink, removeTrailingSlash} from "./blog-meta";
+import { ModeState } from "../store/types";
+import { connect } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "../store/store";
 
 const postsDateList = data["postsDateList"];
 const postsComponentList: JSX.Element[] = [];
@@ -20,32 +24,67 @@ for (const date of postsDateList) {
   );
 }
 
-export default () => {
-  const [num, setNum] = useState(7);
+interface PostsRendererProps {
+  linkcolor: string
+}
+
+const PostsRenderer = (props: PostsRendererProps) => {
+  const [offset, setOffset] = useState(0);
   const article_num = postsComponentList.length;
 
   const show_more = (num: number) => {
-    if (num < article_num) {
-      return (
-        <div className="older" onClick={() => setNum(num + 7)}>
-          <p>older</p>
+    return (
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="post_prev">
+          {(offset !== 0) ? <p><a onClick={() => setOffset(num - 10)}>Prev</a></p> : null}
           <style jsx>{`
-            .older p {
+            .post_prev {
+              display: inline-block;
+              width: 50%;
+            }
+          
+            .post_prev p {
               text-align: left;
               font-style: italic;
               cursor: pointer;
-              color: #4B0082;
+              color: ${props.linkcolor};
+              font-size: 1.1em;
             }
           `}</style>
         </div>
-      );
-    } 
+        <div className="post_next">
+          {(offset + 10 < article_num) ? <p><a onClick={() => setOffset(num + 10)}>Next</a></p> : null}
+          <style jsx>{`
+            .post_next {
+              display: inline-block;
+              width: 50%;                
+            }
+
+            .post_next p {
+              text-align: right;
+              font-style: italic;
+              cursor: pointer;
+              color: ${props.linkcolor};
+              font-size: 1.1em;
+            }
+          `}</style>
+        </div>
+      </PersistGate>
+    );
   };
 
   return (
     <div>
-      {postsComponentList.slice(0, num)}
-      {show_more(num)}
+      {postsComponentList.slice(offset, offset+10)}
+      {show_more(offset)}
     </div>
   );
 };
+
+const mapStateToProps = (state: ModeState) => {
+  return {
+    linkcolor: state.linkcolor
+  };
+};
+
+export default connect(mapStateToProps)(PostsRenderer);
