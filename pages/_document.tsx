da1bdb4af0932893ae632f2a2fb10d4daa60ca84
7ma7X <hellorusk1998@gmail.com>
2020-01-07 04:05:52 +0900

    gtag cut test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,16 +15,6 @@ export default class MyDocument extends Document {
     return (
       <html lang="ja">
         <Head>
-          <script
-            async
-            src={"https://www.googletagmanager.com/gtag/js?id=UA-136305100-1"}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html:
-                "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-136305100-1');"
-            }}
-          />
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta
             name="viewport"
