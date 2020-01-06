5918dad1899d2949612f75115204ed3b42ed44fb
7ma7X <hellorusk1998@gmail.com>
2020-01-07 03:54:10 +0900

    font test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
           <meta name="twitter:site" content="@HelloRusk" />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:300&display=swap"
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:300&display=swap&text=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzá0123456789©%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2D%2E%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E%5F%60%7B%7C%7D%7E"
           />
           <style>{"body { background-color: rgb(21, 32, 43); }"}</style>
         </Head>
